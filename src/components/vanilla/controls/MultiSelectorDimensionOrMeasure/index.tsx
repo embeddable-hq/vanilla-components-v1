372ec5e31ed1619d3b949b14abc44bc970aaf3b7
Rui Ribeiro <57071299+mad-raccoon@users.noreply.github.com>
2025-07-09 10:59:49 +0100

    EM-2016: dimension measure selectors (#84)
    * EM-2016: dimension measure selectors
    
    * Update src/components/vanilla/controls/MultiSelectorMeasure/index.tsx
    
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>
    
    * Update src/components/vanilla/controls/MultiSelectorMeasure/index.tsx
    
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>
    
    * Update src/components/vanilla/controls/MultiSelectorMeasure/index.tsx
    
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>
    
    * Update src/components/vanilla/controls/SelectorDimensionOrMeasure/SelectorDimensionOrMeasure.emb.ts
    
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>
    
    * Update src/components/vanilla/controls/MultiSelectorMeasure/MultiSelectorMeasure.emb.ts
    
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>
    
    * Update src/components/vanilla/controls/SelectorDimension/SelectorDimension.emb.ts
    
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>
    
    * mr review
    
    * fix default values
    
    * improve components with pue functions
    
    * improvements
    
    ---------
    
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

---
+++ src/components/vanilla/controls/MultiSelectorDimensionOrMeasure/index.tsx
@@ -0,0 +1,32 @@
+import { DimensionOrMeasure } from '@embeddable.com/core';
+import React from 'react';
+
+import MultiSelector from '../MultiSelector';
+import { getSelectorOptions } from '../Selector.utils';
+
+export type Props = {
+  allowNoValue?: boolean;
+  defaultValue?: DimensionOrMeasure[];
+  options: DimensionOrMeasure[];
+  title?: string;
+  onChange: (v: DimensionOrMeasure[]) => void;
+};
+
+export default (props: Props) => {
+  const calculatedDefaultValue = props.defaultValue?.map((v) => v.name) || [];
+
+  const handleChange = (newValues: string[]) => {
+    const newSelection = props.options.filter((option) => newValues.includes(option.name));
+    props.onChange(newSelection);
+  };
+
+  return (
+    <MultiSelector
+      title={props.title}
+      options={getSelectorOptions(props.options)}
+      defaultValue={calculatedDefaultValue}
+      unclearable={!props.allowNoValue}
+      onChange={handleChange}
+    />
+  );
+};
