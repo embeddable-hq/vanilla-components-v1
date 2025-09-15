986fe1e3323abde4d66e1e629ba71d423aaf7455
Christopher Buecheler <cwbuecheler@users.noreply.github.com>
2025-09-15 16:56:25 +0200

    CS-143 - Bar Chart Limits (#147)

--- src/components/vanilla/charts/BarChart/BarChart.emb.ts
+++ src/components/vanilla/charts/BarChart/BarChart.emb.ts
@@ -57,6 +57,7 @@ export const meta = {
       name: 'limit',
       type: 'number',
       label: 'Limit results',
+      defaultValue: 100,
       category: 'Chart data',
     },
     {
@@ -201,7 +202,7 @@ export default defineComponent(Component, meta, {
         from: inputs.ds,
         select: [...dimensions, ...measures],
         orderBy: orderProp,
-        limit: inputs.limit || 50,
+        limit: inputs.limit || 100,
       }),
     };
   },
