393b0bf238f8b81832d0b24ae9e82032dfc80d35
Christopher Buecheler <chris@embeddable.com>
2025-09-15 11:45:22 +0200

    account for bundle hash in prod builds

--- src/components/util/getCustomGranularities.ts
+++ src/components/util/getCustomGranularities.ts
@@ -1,15 +1,18 @@
-type NativeTypes = {
+type NativeTypesShape = {
   granularity?: {
     options?: string[];
   };
+};
+
+type Embeddable = {
+  nativeTypes?: NativeTypesShape;
+  // Have to use any here because TS is very confused by the idea that nativeTypes could be top-level OR nested under a bundle hash
   [key: string]: any;
 };
 
 type WindowSimplified = {
+  __EMBEDDABLE__?: Embeddable;
   __EMBEDDABLE_BUNDLE_HASH__?: string;
-  __EMBEDDABLE__?: {
-    [key: string]: NativeTypes; // keyed by bundle hash if it exists, otherwise just nativeTypes
-  };
 };
 
 const extendedWindow = window as WindowSimplified;
@@ -18,22 +21,22 @@ export const getBundleHash = () => {
   return extendedWindow.__EMBEDDABLE_BUNDLE_HASH__;
 };
 
-const getFromWindow = <T>(property: string, fallback: T = {} as T): T => {
+const getFromWindow = (): NativeTypesShape | undefined => {
   const bundleHash = getBundleHash();
   const embeddable = extendedWindow.__EMBEDDABLE__;
 
-  if (bundleHash && embeddable?.[bundleHash as keyof typeof embeddable]?.nativeTypes) {
-    return ((embeddable[bundleHash as keyof typeof embeddable].nativeTypes as NativeTypes)[
-      property
-    ] || fallback) as T;
+  if (bundleHash && embeddable && embeddable[bundleHash]) {
+    const bundleProp = embeddable[bundleHash];
+    return bundleProp.nativeTypes as NativeTypesShape | undefined;
+  } else if (embeddable && embeddable.nativeTypes) {
+    return embeddable.nativeTypes;
   }
-
-  return (embeddable?.[property] || fallback) as T;
+  return undefined;
 };
 
 export const getCustomGranularities = (): string[] => {
   if (typeof window !== 'undefined') {
-    const nativeTypes = getFromWindow<NativeTypes>('nativeTypes', {});
+    const nativeTypes = getFromWindow();
     return nativeTypes?.granularity?.options || [];
   } else {
     return [];
