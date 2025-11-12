524c13b979f9576bb1aa6c5930a4557254b91610
Christopher Buecheler <chris@embeddable.com>
2025-11-12 14:54:52 +0100

    multi dimension line

--- src/components/vanilla/charts/StackedAreaChart/MultiDimensionLineChart.emb.ts
+++ src/components/vanilla/charts/StackedAreaChart/MultiDimensionLineChart.emb.ts
@@ -2,6 +2,7 @@ import { OrderBy, loadData } from '@embeddable.com/core';
 import { EmbeddedComponentMeta, Inputs, defineComponent } from '@embeddable.com/react';
 
 import Component from './index';
+import TimeZones from '../../../../types/TimeZones.type.emb';
 
 export const meta = {
   name: 'MultiDimensionLineChart',
@@ -52,6 +53,14 @@ export const meta = {
       label: 'Granularity',
       defaultValue: 'week',
       category: 'Variables to configure',
+    },
+    {
+      name: 'timezone',
+      type: TimeZones as never,
+      label: 'Time Zone',
+      description: 'The time zone to use for date formatting',
+      category: 'Chart settings',
+      defaultValue: 'UTC',
     },
     {
       name: 'title',
@@ -148,6 +157,7 @@ export default defineComponent(Component, meta, {
           inputs.segment,
           inputs.metric,
         ],
+        timezone: inputs.timezone,
       }),
     };
   },
