a8ee276625822b1b686a02b6cfe6e2c68a15797d
Christopher Buecheler <chris@embeddable.com>
2025-11-12 14:14:54 +0100

    line chart

--- src/components/vanilla/charts/LineChart/LineChart.emb.ts
+++ src/components/vanilla/charts/LineChart/LineChart.emb.ts
@@ -2,6 +2,7 @@ import { OrderBy, loadData } from '@embeddable.com/core';
 import { EmbeddedComponentMeta, Inputs, defineComponent } from '@embeddable.com/react';
 
 import Component from './index';
+import TimeZones from '../../../../types/TimeZones.type.emb';
 
 export const meta = {
   name: 'LineChart',
@@ -44,6 +45,14 @@ export const meta = {
       label: 'Granularity',
       defaultValue: 'day',
       category: 'Variables to configure',
+    },
+    {
+      name: 'timezone',
+      type: TimeZones as never,
+      label: 'Time Zone',
+      description: 'The time zone to use for date formatting',
+      category: 'Chart settings',
+      defaultValue: 'UTC',
     },
     {
       name: 'title',
@@ -158,6 +167,7 @@ export default defineComponent(Component, meta, {
           },
           inputs.metrics,
         ],
+        timezone: inputs.timezone,
       }),
     };
   },
